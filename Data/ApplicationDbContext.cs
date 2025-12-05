25925ea48423fbbb5c9ac85a428b7cca15de1746
SoulThief1322 <alenko2007@gmail.com>
2025-12-05 10:00:34 +0200

    TaskItem model added

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
+using Data.Models;
 namespace TickIt.Data;
 
 public class ApplicationDbContext : IdentityDbContext
@@ -8,5 +8,13 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+    public DbSet<TaskItem> TaskItems { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        
     }
 }
---
+++ Data/Models/TaskItem.cs
@@ -0,0 +1,12 @@
+namespace Data.Models
+{
+    public class TaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public bool IsDone { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? DueDate { get; set; }
+}
+}
